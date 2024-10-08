750f339fbeee4816df65bdb83fd9aa419d7af67d
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-10-08 22:16:15 +0200

    style changes

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Die = (props) => {
   const styles = {
     backgroundColor: props.isHeld ? "#e08439c0" : "#e8e8bfc0",
+    animation: props.isHeld ? "bounce 0.3s" : "none",
   };
 
   return (
